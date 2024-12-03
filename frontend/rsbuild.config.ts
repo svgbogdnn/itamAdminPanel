3a71a2f7b371ac30740e018c5a932d6454b45f3f
Bogdan <115375906+svgbogdnn@users.noreply.github.com>
2024-12-03 20:58:38 +0300

    /

--- frontend/rsbuild.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
-import { pluginStyledComponents } from '@rsbuild/plugin-styled-components';
-
-export default defineConfig({
-  plugins: [pluginReact(), pluginStyledComponents()],
-});
